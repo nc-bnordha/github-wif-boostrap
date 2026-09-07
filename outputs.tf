e549f066a6b000c71e31fa5ab26341198a8bbc2f
Bernd Nordhausen <bernd@nordhausenconsulting.com>
2026-09-07 12:02:43 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,6 @@
-output "workload_identity_provider" {
-  value = google_iam_workload_identity_pool_provider.github.name
-}
-
-output "deployment_service_account" {
-  value = google_service_account.github_deployer.email
-}
-
-output "generated_workflow" {
-  value = abspath(local_file.workflow.filename)
-}
-
-output "next_step" {
-  value = "Copy ${var.workflow_output_path} to .github/workflows/workload.yaml in ${local.repository}"
+output "generated_workflows" {
+  value = {
+    verification = abspath(local_file.verify_workflow.filename)
+    cloud_run    = abspath(local_file.cloud_run_workflow.filename)
+  }
 }
